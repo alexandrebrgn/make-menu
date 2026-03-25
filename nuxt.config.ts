82f6531a516039c712401abfc7b5e74a0d93d05b
Alexandre BOURGUIGNON <abourguignon@puydufou.com>
2026-03-25 01:31:31 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devServer: {
+    port: 8000,
+    host: '0.0.0.0'
+  },
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
