0a98e60cb2d02b69e250ece6a71b2afb3930c213
Alexandre BOURGUIGNON <abourguignon@puydufou.com>
2026-03-28 03:01:20 +0100

    feat: add breadcrumb and menutoggle

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,18 +3,29 @@ import { useAuthStore } from '~/stores/auth';
 
 const { t } = useI18n();
 const auth = useAuthStore();
+const route = useRoute()
+const toggledBar = ref(true)
 
 const menu = [
-    { name: t('home'), link: '/', icon: 'solar:home-outline' },
-    { name: t('fridge'), link: '/fridge', icon: 'solar:fridge-outline' },
-    { name: t('recipes'), link: '/recipes', icon: 'solar:book-outline' },
-    { name: t('kitchen'), link: '/kitchen', icon: 'solar:oven-mitts-linear' },
+  { name: t('home'), link: '/', icon: 'solar:home-outline' },
+  { name: t('fridge'), link: '/fridge', icon: 'solar:fridge-outline' },
+  { name: t('recipes'), link: '/recipes', icon: 'solar:book-outline' },
+  { name: t('kitchen'), link: '/kitchen', icon: 'solar:oven-mitts-linear' },
 ]
 
 const handleLogout = () => {
-    auth.logout()
+  auth.logout()
 }
 
+const breadcrumb = computed(() => {
+  return route.path
+    .split('/')
+    .filter(Boolean)
+})
+
+const handleToggleSidebar = () => {
+  toggledBar.value = !toggledBar.value
+}
 
 </script>
 
@@ -23,30 +34,50 @@ const handleLogout = () => {
     <VitePwaManifest />
 
     <!-- Header / Nav -->
-    <nav class="border-r min-w-1/6 p-4 h-full">
+    <nav class="border-r h-full overflow-hidden transition-all duration-300"
+      :class="toggledBar ? 'w-64 p-4' : 'w-20 p-4'">
       <div class="flex flex-col justify-between h-full">
         <div class="flex flex-col gap-8">
-          <div class="flex gap-2 items-center">
-            <div class="h-8 w-8 rounded-lg bg-black"></div>
-            <h1 class="text-xl font-bold">Make Menu</h1>
+          <!-- Logo -->
+          <div class="flex gap-2 items-center" :class="toggledBar ? '' : 'justify-center'">
+            <div class="rounded-lg bg-black" :class="toggledBar ? 'h-8 w-8' : 'h-8 w-6'"></div>
+            <h1 v-if="toggledBar" class="text-xl font-bold">Make Menu</h1>
           </div>
-          <div class="flex flex-col gap-2 pl-2">
-            <NuxtLink v-for="item in menu" :to="item.link" class="text-gray-600 rounded-md pl-2 py-1 flex items-center gap-2 hover:bg-gray-50 hover:text-black " :key="item.link">
-              <Icon :name="item.icon" style="color: black" />
-              <p>{{ item.name }}</p>
+          <!-- Menu Items -->
+          <div class="flex flex-col gap-2" :class="toggledBar ? 'pl-2' : 'pl-0'">
+            <NuxtLink v-for="item in menu" :to="item.link"
+              class="text-gray-600 rounded-md py-1 flex items-center gap-2 hover:bg-gray-50 hover:text-black"
+              :class="toggledBar ? 'pl-2' : 'pl-0 justify-center'"
+              :key="item.link">
+              <Icon :name="item.icon" />
+              <p v-if="toggledBar">{{ item.name }}</p>
             </NuxtLink>
           </div>
         </div>
-        <button class="w-full bg-black rounded-lg flex items-center justify-center text-white py-1" @click="handleLogout">
-          <Icon name="solar:logout-2-outline" />
-          <span class="ml-2">Se déconnecter</span>
+        <!-- Logout Button -->
+        <button class="w-full bg-black rounded-lg flex items-center justify-center text-white py-1"
+          @click="handleLogout">
+          <Icon name="solar:logout-3-outline" />
+          <span class="ml-2 transition-all duration-200" :class="toggledBar ? 'inline' : 'hidden'">Se déconnecter</span>
         </button>
       </div>
     </nav>
 
     <!-- Main -->
     <main class="flex-1 overflow-y-auto">
-      <slot />
+      <div class="w-full flex items-center p-4 gap-2">
+        <Icon name="solar:sidebar-minimalistic-linear" style="color: black" size="20" @click="handleToggleSidebar" class="cursor-pointer"/>
+        <Icon name="solar:alt-arrow-right-outline" style="color: black" size="20" />
+        <div class="flex items-center gap-2">
+          <template v-for="(part, index) in breadcrumb" :key="index">
+            <p class="text-md">{{ t(part) }}</p>
+
+            <Icon v-if="index < breadcrumb.length - 1" name="solar:alt-arrow-right-outline" size="16" />
+          </template>
+        </div>
+        <p> <!-- Nom de la route a remplir de puis le store ou autre --></p>
+      </div>
+      <slot/>
     </main>
 
     <!-- Footer -->
---
+++ app/pages/fridge/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useFridgeStore } from '~/stores/fridge';
+
+const { t } = useI18n();
+const { $pwa } = useNuxtApp();
+const fridgeStore = useFridgeStore();
+
+const handleInstallPwa = (() => {
+    useNuxtApp().$pwa?.install()
+})
+
+</script>
+
+<template>
+    <div class="min-h-screen px-4">
+        <button v-if="$pwa?.showInstallPrompt" @click="handleInstallPwa"
+            class="bg-black p-2 text-white border border-gray-400 rounded-lg">
+            Install app
+        </button>
+        <p class="text-2xl text-black font-bold">{{ t('fridge') }}</p>
+    </div>
+</template>
